4c362d742f014e0f558b6e60e019938f67ca29ec
Ignacio <nachopallaj@gmail.com>
2019-01-31 23:28:34 +0100

    Header & delete alert

--- ionicApp/src/app/tab1/tab1.page.ts
+++ ionicApp/src/app/tab1/tab1.page.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(public alertController: AlertController){}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Confirm',
+      message: 'Are you sure you want to delete the device?',
+      buttons: ['Yes', 'Cancel']
+    });
+
+    await alert.present();
+  }
+}
